376cf33643bbd61310674a13f241b2e54ce331a2
潘博 <tianzhizi575@hotmail.com>
2019-08-10 16:51:35 +0800

    更新后端接口地址

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,2 +1,2 @@
-const URL = process.env.NODE_ENV === 'production' ? 'http://192.168.148.129:8088/' : 'http://localhost:21021/';
+const URL = process.env.NODE_ENV === 'production' ? 'http://192.168.148.136:80/' : 'http://localhost:21021/';
 export default URL;
